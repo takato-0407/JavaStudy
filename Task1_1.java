6e8ac38884f747e0874923de9832f78db9cdbc9a
takato-0407 <70625268+takato-0407@users.noreply.github.com>
2020-12-03 22:39:02 +0900

    Initial commit

---
+++ Task1_1.java
@@ -0,0 +1,28 @@
+public class Task1_1 {
+
+    public static void main(String[] args) {
+
+        int hogeInt = 50;
+        
+        // ②Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化
+        String hogeString = "テスト";
+        
+        // ③下記の処理について、何をしているのかコメントを記入してください
+        // [ここへ記述]  浮動小数点型のdoubleにhogeDoubleと言う変数を作り0.2を代入した
+        double hogeDouble = 0.2;
+
+        // ④変数 hogeInt を値「100」で上書き
+        int hogeInt = 100;
+        // ⑤変数 hogeString を値「合格」で上書き
+        String hogeString = "合格";
+        // ⑥変数 hogeInt の値を表示
+        System.out.println(hogeInt);
+        // ⑦下記の処理について、何をしているのかコメントを記入してください
+        // [ここへ記述]hogeStringの値を出力している
+        System.out.println(hogeString);
+
+        // ⑧変数 hogeDouble の値を表示
+        System.out.println(hogeDouble);
+
+    }
+}
